4a88164ca843672c9c926dc1f8253966f2a58d63
Maksim <knyazev.maksim2852@gmail.com>
2025-01-31 21:39:33 +0400

    добавлены jwt исключения

---
+++ src/domain/jwt/exception.py
@@ -0,0 +1,18 @@
+from src.domain.common.exception import DomainErrorException
+from fastapi import status
+
+
+# JWT token
+class TokenExpiredException(DomainErrorException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Токен истёк'
+
+
+class TokenAbsentException(DomainErrorException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Токен отсутствует'
+
+
+class IncorrectTokenException(DomainErrorException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    detail = 'Неверный формат токена'
