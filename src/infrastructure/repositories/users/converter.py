1087f4525905037ebd4818741dd4e1bce67e269c
Maksim <knyazev.maksim2852@gmail.com>
2025-01-31 21:37:18 +0400

    обновлены функции

--- src/infrastructure/repositories/users/converter.py
+++ src/infrastructure/repositories/users/converter.py
@@ -1,3 +1,4 @@
+from typing import Any, Mapping
 from src.domain.user.entity import User
 
 
@@ -9,3 +10,22 @@ def convert_user_entity_to_document(user: User, hashed_password: str) -> dict:
         'created_at': user.created_at,
         'hashed_password': hashed_password,
     }
+    
+
+def convert_document_to_user_entity(document: Mapping[str, Any]) -> User:
+    return User(
+        uuid=document['uuid'],
+        username=document['username'],
+        email=document['email'],
+        created_at=document['created_at'],
+    )
+    
+
+
+def convert_document_to_user_entity_with_hashed_password(document: Mapping[str, Any]) -> User:
+    return User(
+        uuid=document['uuid'],
+        username=document['username'],
+        email=document['email'],
+        created_at=document['created_at'],
+    ), document['hashed_password']
