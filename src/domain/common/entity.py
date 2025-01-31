70dd7ca83fea65c060960984dd7e472f413b6fee
Maksim <knyazev.maksim2852@gmail.com>
2025-01-31 10:51:21 +0400

    добавлено поле created_at

--- src/domain/common/entity.py
+++ src/domain/common/entity.py
@@ -1,24 +1,22 @@
 from abc import ABC
-from copy import copy
 from dataclasses import dataclass, field
+from uuid import uuid4
 from datetime import datetime
 
 
 @dataclass(kw_only=True)
 class BaseEntity(ABC):
     
-    id: int | None = field(
-        default=None,
+    uuid: str = field(
+        default_factory=lambda: str(uuid4),
     )
     created_at: datetime = field(
         default_factory=datetime.now,
+        kw_only=True,
     )
-    updated_at: datetime = field(
-        default_factory=datetime.now,
-    )
-
+    
     def __hash__(self) -> int:
-        return hash(self.id)
+        return hash(self.uuid)
 
     def __eq__(self, __value: 'BaseEntity') -> bool:
-        return self.id == __value.id
+        return self.uuid == __value.uuid
