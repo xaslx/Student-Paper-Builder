c6aa0d24bc8e27fbb0772dec5fe5f243480b2e00
Maksim <knyazev.maksim2852@gmail.com>
2025-01-31 21:37:02 +0400

    обновлены/добавлены новые методы

--- src/infrastructure/repositories/users/base.py
+++ src/infrastructure/repositories/users/base.py
@@ -17,3 +17,11 @@ class BaseUsersRepository(ABC):
     @abstractmethod
     async def add_user(self, user: User, hashed_password: str) -> None:
         ...
+        
+    @abstractmethod
+    async def exists_user(self, email: str) -> bool:
+        ...
+        
+    @abstractmethod
+    async def get_user_by_username(self, username: str) -> User | None:
+        ...
--- src/infrastructure/repositories/users/mongo.py
+++ src/infrastructure/repositories/users/mongo.py
@@ -1,7 +1,7 @@
 from abc import ABC
 from dataclasses import dataclass
 from motor.core import AgnosticClient
-from src.infrastructure.repositories.users.converter import convert_user_entity_to_document
+from src.infrastructure.repositories.users.converter import convert_document_to_user_entity_with_hashed_password, convert_user_entity_to_document, convert_document_to_user_entity
 from src.domain.user.entity import User
 from src.infrastructure.repositories.users.base import BaseUsersRepository
 
@@ -24,10 +24,27 @@ class BaseMongoDBRepository(ABC):
 class MongoDBUsersRepository(BaseUsersRepository, BaseMongoDBRepository):
     
     async def get_user_by_uuid(self, uuid: str) -> User | None:
-        user_document = self._collection.find_one(filter={'uuid': uuid})
+        user_document = await self._collection.find_one(filter={'uuid': uuid})
         
         if not user_document:
             return None
 
+        return convert_document_to_user_entity(document=user_document)
+
     async def add_user(self, user: User, hashed_password: str) -> None:
         await self._collection.insert_one(convert_user_entity_to_document(user=user, hashed_password=hashed_password))
+        
+    async def exists_user(self, email: str, username: str) -> bool:
+        user_document = await self._collection.find_one(
+            {'$or': [{'email': email}, {'username': username}]},
+            {'_id': 1}
+        )
+        return bool(user_document)
+
+    
+    async def get_user_by_username(self, username: str) -> User | None:
+        user_document = await self._collection.find_one(filter={'username': username})
+        if not user_document:
+            return None
+        
+        return convert_document_to_user_entity_with_hashed_password(document=user_document)
