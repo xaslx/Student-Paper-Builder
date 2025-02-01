cef9f8e117e931281d417de7d63cefa7caf1ad5d
Maksim <knyazev.maksim2852@gmail.com>
2025-02-01 17:59:48 +0400

    обновлены схемы для регистрации пользователя

--- src/presentation/schemas/user.py
+++ src/presentation/schemas/user.py
@@ -1,10 +1,30 @@
-from pydantic import BaseModel, EmailStr
+import re
+
+from pydantic import BaseModel, EmailStr, Field, field_validator
+
+from src.domain.user.exception import InvalidUsernameOrPasswordException, PasswordLengthException
+
+
+PASSWORD_CHECK = re.compile(r"^[\w!\"#$%&'()*+,\-./:;<=>?@\[\\\]^_`{|}~]{6,30}$")
 
 
 class UserRegister(BaseModel):
     username: str
     email: EmailStr
     password: str
+    
+    @field_validator('password')
+    def validate_password(cls, value: str) -> str:
+        
+        if not PASSWORD_CHECK.match(value):
+            raise InvalidUsernameOrPasswordException()
+        
+        if not (6 <= len(value) <= 30):
+            raise PasswordLengthException()
+        
+        return value
+    
+    
     
 
 class UserOut(BaseModel):
@@ -15,3 +35,7 @@ class UserOut(BaseModel):
 class UserLogin(BaseModel):
     username: str
     password: str
+    
+
+class ResetPassword(BaseModel):
+    email: EmailStr
