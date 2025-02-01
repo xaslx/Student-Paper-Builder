bb21872b45d353c95cf93b8f49df48dbf74d87e1
Maksim <knyazev.maksim2852@gmail.com>
2025-02-01 17:56:51 +0400

    изменены конвертеры

--- src/infrastructure/repositories/users/converter.py
+++ src/infrastructure/repositories/users/converter.py
@@ -1,11 +1,12 @@
 from typing import Any, Mapping
 from src.domain.user.entity import User
+from src.domain.user.value_object import Username
 
 
 def convert_user_entity_to_document(user: User, hashed_password: str) -> dict:
     return {
         'uuid': user.uuid,
-        'username': user.username,
+        'username': user.username.to_raw(),
         'email': user.email,
         'created_at': user.created_at,
         'hashed_password': hashed_password,
@@ -15,7 +16,7 @@ def convert_user_entity_to_document(user: User, hashed_password: str) -> dict:
 def convert_document_to_user_entity(document: Mapping[str, Any]) -> User:
     return User(
         uuid=document['uuid'],
-        username=document['username'],
+        username=Username(value=document['username']),
         email=document['email'],
         created_at=document['created_at'],
     )
@@ -25,7 +26,7 @@ def convert_document_to_user_entity(document: Mapping[str, Any]) -> User:
 def convert_document_to_user_entity_with_hashed_password(document: Mapping[str, Any]) -> User:
     return User(
         uuid=document['uuid'],
-        username=document['username'],
+        username=Username(value=document['username']),
         email=document['email'],
         created_at=document['created_at'],
     ), document['hashed_password']
