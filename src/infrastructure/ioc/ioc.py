dbd1175dcdacf8e7be458915b396c59b4e8e6fa2
Maksim <knyazev.maksim2852@gmail.com>
2025-02-02 16:11:22 +0400

    зарегистрированы новые юз кейсы, репозитории, зависимости

--- src/infrastructure/ioc/ioc.py
+++ src/infrastructure/ioc/ioc.py
@@ -3,6 +3,11 @@ from fastapi import Request
 from fastapi.templating import Jinja2Templates
 from motor.motor_asyncio import AsyncIOMotorClient
 
+from src.application.use_cases.documents.get_document import GetDocumentUseCase
+from src.application.use_cases.documents.delete import DeleteAllDocumentsUseCase, DeleteDocumentUseCase
+from src.domain.document.entity import Document
+from src.infrastructure.repositories.documents.mongo import MongoDBDocumentRepository
+from src.infrastructure.repositories.documents.base import BaseDocumentsRepository
 from src.application.services.auth import AuthServiceImpl, BaseAuthService
 from src.application.services.hash import BaseHashService, BCryptHashService
 from src.application.services.jwt import JWTService, JWTServiceImpl
@@ -13,6 +18,8 @@ from src.domain.user.entity import User
 from src.infrastructure.repositories.users.base import BaseUsersRepository
 from src.infrastructure.repositories.users.mongo import MongoDBUsersRepository
 from src.settings.config import Config
+from src.application.use_cases.documents.create import CreateDocumentUseCase
+
 
 
 class AppProvider(Provider):
@@ -38,6 +45,15 @@ class AppProvider(Provider):
             mongo_db_client=client,
             mongo_db_db_name=config.mongodb.mongodb_database,
             mongo_db_collection_name=config.mongodb.mongodb_users_collection,
+        )
+        
+    @provide(scope=Scope.REQUEST)
+    def init_documents_mongodb_repository(self, client: AsyncIOMotorClient, config: Config) -> BaseDocumentsRepository:
+        
+        return MongoDBDocumentRepository(
+            mongo_db_client=client,
+            mongo_db_db_name=config.mongodb.mongodb_database,
+            mongo_db_collection_name=config.mongodb.mongodb_documents_collection,
         )
 
     #services
@@ -115,6 +131,36 @@ class AppProvider(Provider):
             hash_service=hash_service,
         )    
 
+    @provide(scope=Scope.REQUEST)
+    def get_create_document_use_case(
+        self,
+        document_repository: BaseDocumentsRepository,
+    ) -> CreateDocumentUseCase:
+        
+        return CreateDocumentUseCase(document_repository=document_repository)
+    
+    @provide(scope=Scope.REQUEST)
+    def get_delete_document_use_case(
+        self,
+        document_repository: BaseDocumentsRepository,
+    ) -> DeleteDocumentUseCase:
+        return DeleteDocumentUseCase(document_repository=document_repository)
+
+
+    @provide(scope=Scope.REQUEST)
+    def get_delete_all_documents_use_case(
+        self,
+        document_repository: BaseDocumentsRepository,
+    ) -> DeleteAllDocumentsUseCase:
+        return DeleteAllDocumentsUseCase(document_repository=document_repository)
+
+
+    @provide(scope=Scope.REQUEST)
+    def get_document_by_uuid_use_case(
+        self,
+        document_repository: BaseDocumentsRepository,
+    ) -> GetDocumentUseCase:
+        return GetDocumentUseCase(document_repository=document_repository)
 
     #dependencies
     @provide(scope=Scope.REQUEST)
@@ -126,6 +172,7 @@ class AppProvider(Provider):
             return None
         return token
 
+
     @provide(scope=Scope.REQUEST)
     async def get_current_user_dependency(
         self,
@@ -136,3 +183,14 @@ class AppProvider(Provider):
         if not token:
             return None
         return await auth_service.get_current_user(token=token)
+    
+    
+    @provide(scope=Scope.REQUEST)
+    async def get_all_documents_for_user(
+        self,
+        document_repository: BaseDocumentsRepository,
+        current_user: User,
+    ) -> list[Document]:
+        
+        if current_user:
+            return await document_repository.get_all_documents_by_user_uuid(current_user.uuid)
