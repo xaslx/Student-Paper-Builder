00402479b9a5e05b633977822b90660547d36167
Maksim <knyazev.maksim2852@gmail.com>
2025-02-02 16:14:52 +0400

    зарегистрирован новый роутер

--- src/presentation/controllers/setup_routers.py
+++ src/presentation/controllers/setup_routers.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from src.presentation.controllers.main import router as main_router
 from src.presentation.controllers.auth import router as auth_router
+from src.presentation.controllers.document import router as document_router
 
 
 def setup_controllers(app: FastAPI) -> None:
     app.include_router(main_router)
     app.include_router(auth_router)
+    app.include_router(document_router)
